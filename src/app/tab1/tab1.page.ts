cbb094cb26dd0492671b824f8c7a323b6ef84ba2
joaogabrielramos <bielzinhobk@hotmail.com>
2020-07-27 14:45:57 -0300

    hommy-app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { IonIcon } from '@ionic/angular';
+class republica{
+  image:string;
+  title:string;
+  subtitle:string;
+  content:string;
+  more:string;
+
+}
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +15,60 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  republicas:republica[];
+  
   constructor() {}
+  
 
+  ngOnInit(){
+    this.republicas = [
+      {
+      image:"assets/img/dorm2.jpg",
+      title:"Quarto compartilhado - Tijuca",
+      subtitle:"Republica",
+      content:"Situada na Rua Tijuquinha número 123, Rio de Janeiro",
+      more:"Mais...",
+      },
+      {
+      image:"assets/img/dorm.jpg",
+      title:"Quarto individual - Ilha do Governador",
+      subtitle:"Republica",
+      content:"Situada na Rua Ilhazinha número 457,Rio de Janeiro",
+      more:"Mais...",
+      },
+      {
+      image:"assets/img/dorm.jpg",
+      title:"Quarto individual - Ilha do Governador",
+      subtitle:"Republica",
+      content:"Situada na Rua Ilhazinha número 457,Rio de Janeiro",
+      more:"Mais...",
+        
+      },
+      {
+      image:"https://images.unsplash.com/photo-1576095910326-9de5a8b207e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      title:"Quarto individual - Barra da Tijuca",
+      subtitle:"Republica",
+      content:"Situada na Rua Barrinha número 223, Rio de Janeiro",
+      more:"Mais...",
+      },
+      {
+      image:"https://images.unsplash.com/photo-1588750653876-52ad8a14af1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      title:"Quarto individual - Seropédica",
+      subtitle:"Republica",
+      content:"Situada na Rua Seropedinha número 220, Rio de Janeiro",
+      more:"Mais...",
+
+      },
+      {
+        image:"https://images.unsplash.com/photo-1574716236621-87d2be17b3a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        title:"Quarto compartilhado - Niterói",
+        subtitle:"republica",
+        content:"Situada na Rua Niteróizinha número 447, Rio de Janeiro",
+        more:"Mais...",
+
+
+      }
+     
+    ]}
+    
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+class favorito{
+  image:string;
+  title:string;
+  subtitle:string;
+  content:string;
+}
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  favoritos:favorito[];
 
   constructor() {}
 
+  ngOnInit(){
+    this.favoritos = [
+      {
+      image:"assets/img/dorm2.jpg",
+      title:"Quarto compartilhado - Tijuca",
+      subtitle:"Republica",
+      content:"Situada na Rua Tijuquinha número 123, Rio de Janeiro. Contas inclusas(água,luz,gás), preço mensal:R$300.00 reais. Permitidos:Crianças e animais. Proibido :Fumar "
+       
+      
+      },
+      {
+      image:"https://images.unsplash.com/photo-1588750653876-52ad8a14af1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      title:"Quarto individual - Seropédica",
+      subtitle:"Republica",
+      content:"Situada na Rua Seropedinha número 220, Rio de Janeiro. Contas não inclusas, preço mensal:R$150.00. Permitidos: Crianças, Proibido:Fumar e animais.",
+      
+     
+      },
+      
+     
+    ]}
 }
