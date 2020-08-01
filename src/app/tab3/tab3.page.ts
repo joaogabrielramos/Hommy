d3d9d200004ffb87643c0e6932742cc198e90343
joaogabrielramos <bielzinhobk@hotmail.com>
2020-07-31 23:54:40 -0300

    interpolação

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-class favorito{
+class favoritos{
   image:string;
   title:string;
   subtitle:string;
   content:string;
+  kitchenComplete:Boolean;
+  freeWifi:boolean;
+  bedroomComplete:boolean;
+  freeBedroom:number;
+  contas:boolean;
+  ownerName:string;
+  ownerPhoto:string;
+
 }
 
 @Component({
@@ -12,29 +20,27 @@ class favorito{
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-  favoritos:favorito[];
+ favoritos:any;
 
   constructor() {}
 
   ngOnInit(){
-    this.favoritos = [
+    this.favoritos = 
       {
       image:"assets/img/dorm2.jpg",
       title:"Quarto compartilhado - Tijuca",
       subtitle:"Republica",
-      content:"Situada na Rua Tijuquinha número 123, Rio de Janeiro. Contas inclusas(água,luz,gás), preço mensal:R$300.00 reais. Permitidos:Crianças e animais. Proibido :Fumar "
-       
-      
-      },
-      {
-      image:"https://images.unsplash.com/photo-1588750653876-52ad8a14af1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      title:"Quarto individual - Seropédica",
-      subtitle:"Republica",
-      content:"Situada na Rua Seropedinha número 220, Rio de Janeiro. Contas não inclusas, preço mensal:R$150.00. Permitidos: Crianças, Proibido:Fumar e animais.",
+      content:"Situada na Rua Tijuquinha número 123, Rio de Janeiro. Preço mensal:R$500.00 reais. Permitidos:Crianças e animais. Proibido :Fumar ",
+      kitchenComplete:true,
+      freeWifi:true,
+      bedroomComplete:true,
+      freeBedroom:2,
+      contas:true,
+      ownerName:"samuel",
+      ownerPhoto:"../assets/img/profile.png"
+      }
+     
       
      
-      },
-      
-     
-    ]}
+    }
 }
